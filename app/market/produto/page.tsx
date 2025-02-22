efb5ed2088aa2065da2aa47db82f83b1ce2e4afe
Vincenzo Bonatti <vincenzobonati@outlook.com>
2025-02-21 22:21:45 -0300

    conexão com api funcionando

--- app/market/produto/page.tsx
+++ app/market/produto/page.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image'
 import imagem from '/public/mesa.png'
 
-export default function Produto(){
+export default async function Produto(){
+
+
+    let a = await fetch("http://localhost:3333")
+    let b = await a.json()
+    console.log(b)
 
     return(
         <div className='flex flex-col justify-center items-center gap-5 bg-black w-fit p-3 rounded-[8%]'>
@@ -10,6 +15,7 @@ export default function Produto(){
             <p>Mesa retratil</p>
             <p>Estoque: xx</p>
             <p>Preço: xx,xx</p> 
+            {b.hello}
             </div>
         </div>
     )
